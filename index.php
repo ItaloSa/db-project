b7971000928586f18d82feedd5509780906b169c
Italo Sousa <italusousa@gmail.com>
2019-01-03 18:57:46 -0200

    Add dotenv

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once "vendor/autoload.php";
 
 use app\Router as Router;
+use Dotenv\Dotenv as Dotenv;
+
+$dotenv = Dotenv::create(__DIR__);
+$dotenv->load();
 
 $app = new \Slim\App;
 
